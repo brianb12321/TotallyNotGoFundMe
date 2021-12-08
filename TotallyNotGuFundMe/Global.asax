63d89e9f125e7438c2a87d743bf6dde011207948
brianb12321 <brian.jx.b@gmail.com>
2021-12-07 18:07:10 -0600

    Initialized repository

---
+++ TotallyNotGuFundMe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TotallyNotGuFundMe.Global" Language="C#" %>
---
+++ TotallyNotGuFundMe/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TotallyNotGuFundMe.Startup))]
+namespace TotallyNotGuFundMe
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
